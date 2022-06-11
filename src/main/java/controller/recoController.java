cc18c2ea2fccb05136579dd80bfcb28047a29495
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-06-11 11:15:28 +0800

    Basic Controller actions

--- src/main/java/controller/recoController.java
+++ src/main/java/controller/recoController.java
@@ -1,5 +1,39 @@
 package controller;
 
-public class recoController {
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/recommend")
+public class RecoController {
     
+    @Autowired
+    RecomRepository Rrepo;
+
+    @RequestMapping("/list")
+    public String list(Model model){
+
+        return "ratinglist";
+    }
+
+    @RequestMapping("/add")
+    public String addForm(Model model){
+        return "rating-form";
+    }
+
+    @RequestMapping("/save")
+    public String saveRatings(){
+
+        return "forward:/recommend/list";
+    }
+
+
 }
