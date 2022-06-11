6ab6feb5be833252c7f481131ed205c526ce9729
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-06-11 10:38:12 +0800

    first commit

---
+++ src/main/java/YCEM/MovieRecoSystem/MovieRecoSystemApplication.java
@@ -0,0 +1,13 @@
+package YCEM.MovieRecoSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecoSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/YCEM/MovieRecoSystem/MovieRecoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package YCEM.MovieRecoSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecoSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
