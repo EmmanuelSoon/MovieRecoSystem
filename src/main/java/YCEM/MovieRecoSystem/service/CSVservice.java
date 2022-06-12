e93b667e95fc5f2bccbbe061b3f5aaee458f22e1
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-06-12 13:36:11 +0800

    csv intialization testing

--- src/main/java/YCEM/MovieRecoSystem/service/CSVservice.java
+++ src/main/java/YCEM/MovieRecoSystem/service/CSVservice.java
@@ -3,11 +3,12 @@ package YCEM.MovieRecoSystem.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import YCEM.MovieRecoSystem.model.*;
 import YCEM.MovieRecoSystem.repo.*;
 
-
+@Service
 public class CSVservice {
 
     @Autowired
--- src/test/java/YCEM/MovieRecoSystem/MovieRecoSystemApplicationTests.java
+++ src/test/java/YCEM/MovieRecoSystem/MovieRecoSystemApplicationTests.java
@@ -1,13 +1,20 @@
 package YCEM.MovieRecoSystem;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import YCEM.MovieRecoSystem.service.CSVservice;
 
 @SpringBootTest
 class MovieRecoSystemApplicationTests {
 
+	@Autowired
+	CSVservice fileService;
+
 	@Test
 	void contextLoads() {
+		fileService.csvInit();
 	}
 
 }
